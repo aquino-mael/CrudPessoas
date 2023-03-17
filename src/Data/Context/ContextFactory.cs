dc2f9635be3cbb8d93423d5442b27a18341d2c56
Ismael Aquino <ismaellAquino@hotmail.com>
2023-03-17 18:24:47 -0300

    feat: create ContextFactory class

---
+++ src/Data/Context/ContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=dbPessoaCrud;Uid=root;Pwd=Ismael-753951456654!";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySQL(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
