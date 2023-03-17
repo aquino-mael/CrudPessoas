973db14a6522b211be933db346ba511cf5e0f1f4
Ismael Aquino <ismaellAquino@hotmail.com>
2023-03-17 19:05:04 -0300

    feat: add PessoaMap model builder entity configuration

--- src/Data/Context/MyContext.cs
+++ src/Data/Context/MyContext.cs
@@ -1,3 +1,4 @@
+using Data.Mapping;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,5 +9,11 @@ namespace Data.Context
         public MyContext(DbContextOptions<MyContext> options) : base(options) { }
 
         public DbSet<PessoaEntity>? Pessoas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PessoaEntity>(new PessoaMap().Configure);
+        }
     }
 }
