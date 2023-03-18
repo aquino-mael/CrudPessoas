19f7b200902cb151a952ceef24f9510d339c807f
Ismael Aquino <ismaellAquino@hotmail.com>
2023-03-18 04:12:29 -0300

    feat: add configurations of Dependencies Injection on Program

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -1,9 +1,27 @@
+using AutoMapper;
+using CrossCutting.DependencyInjection;
+using CrossCutting.Mappings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+ConfigureContext.ConfigureDependenciesService(builder.Services);
+ConfigureService.ConfigureDependenciesService(builder.Services);
+ConfigureRepository.ConfigureDependenciesService(builder.Services);
+
+var mapperConfiguration = new AutoMapper.MapperConfiguration(
+    cfg => cfg.AddProfile(
+        new PessoaProfile()
+    )
+);
+
+IMapper mapper = mapperConfiguration.CreateMapper();
+
+builder.Services.AddSingleton(mapper);
 
 var app = builder.Build();
 
