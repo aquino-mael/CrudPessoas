8781a75b1735c5e03721291931b66dc41db992c2
Ismael Aquino <ismaellAquino@hotmail.com>
2023-03-18 04:09:51 -0300

    feat: create configureContext

---
+++ src/CrossCutting/DependencyInjection/ConfigureContext.cs
@@ -0,0 +1,17 @@
+﻿using Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.DependencyInjection;
+
+public class ConfigureContext
+{
+    public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddDbContext<MyContext>(
+            option => option.UseMySQL(
+                "Server=localhost;Port=3306;Database=dbPessoaCrud;Uid=root;Pwd=Ismael-753951!"
+            )
+        );
+    }
+}
