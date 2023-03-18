4e97dace2660cf8e44e587ca4f168af7f64f5551
Ismael Aquino <ismaellAquino@hotmail.com>
2023-03-18 04:10:20 -0300

    feat: create IPessoaService

---
+++ src/Domain/Interfaces/Services/Pessoas/IPessoaService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Dtos.Pessoa;
+
+namespace Domain.Interfaces.Services.Users;
+
+public interface IPessoaService
+{
+    Task<PessoaDtoResult?> Get(Guid id);
+    Task<IEnumerable<PessoaDtoResult>?> GetAll();
+    Task<PessoaCreateResultDto?> Post(PessoaDto pessoa);
+    Task<PessoaUpdateResultDto?> Put(PessoaUpdateDto pessoa);
+    Task<bool> Delete(Guid id);
+}
