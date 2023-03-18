07899d155004e9f8ad7eb9d962c53d1f4e989017
Ismael Aquino <ismaellAquino@hotmail.com>
2023-03-18 04:10:00 -0300

    feat: create ConfigureRepository

---
+++ src/CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,13 @@
+﻿using Data.Repository;
+using Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.DependencyInjection;
+
+public class ConfigureRepository
+{
+    public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+    }
+}
